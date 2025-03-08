aebad6ad4aeebf37227105c4f8490e40a414d4fb
Syreel <fede.bonaccorsi99@gmail.com>
2025-03-08 19:29:38 +0100

    tried different cycle alternative for quadprog and subgradient method

--- svr_test_1.m
+++ svr_test_1.m
@@ -1,6 +1,7 @@
 clc; clear; close all;
 
 [X, y] = training_data("abalone");
+X = zscore(X);
 
 kernel_function = RBFKernel();
 lbm = LBM(500, 1e-6, 0.1, 0.5, 20, 0);
@@ -17,9 +18,9 @@ y_pred = svr.predict(X);
 
 disp(mse(y_pred, y));
 
-figure; hold on;
-plot(X, y, '-', 'LineWidth', 1, 'DisplayName', 'Dati training');
-plot(X, y_pred, 'r-', 'LineWidth', 2, 'DisplayName', 'Predizione SVR');
-scatter(X_sv, Y_sv, 'ks', 'MarkerFaceColor', 'g', 'DisplayName', 'Support Vectors');
-xlabel('X'); ylabel('y'); title('SVR Duale (RBF) con Bundle Method');
-legend; grid on;
+% figure; hold on;
+% plot(X, y, '-', 'LineWidth', 1, 'DisplayName', 'Dati training');
+% plot(X, y_pred, 'r-', 'LineWidth', 2, 'DisplayName', 'Predizione SVR');
+% scatter(X_sv, Y_sv, 'ks', 'MarkerFaceColor', 'g', 'DisplayName', 'Support Vectors');
+% xlabel('X'); ylabel('y'); title('SVR Duale (RBF) con Bundle Method');
+% legend; grid on;
